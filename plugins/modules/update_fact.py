30515267141cee9567b54927877786cd8cd1e89e
cidrblock <brad@thethorntons.net>
2020-09-18 13:55:30 -0700

    Docstring fix

--- plugins/modules/update_fact.py
+++ plugins/modules/update_fact.py
@@ -12,11 +12,10 @@ description:
     - This module allows updating existing variables.
     - Variables are updated on a host-by-host basis.
     - Variable are not modified in place, instead they are returned by the module
-    - This module is also supported for Windows targets.
 options:
   key_value:
     description:
-      - The C(set_fact) module takes key=value pairs as variables to update
+      - The C(update_fact) module takes key=value pairs as variables to update
       - The key should be in dot or bracket notation, indicating the path to update
       - The key value should be a valid jinja reference
       - Exisiting keys or list entries will be replaced by the value provided.

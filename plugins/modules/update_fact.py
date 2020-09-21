dde7816cda60621347441fa4541151453c4579ec
cidrblock <brad@thethorntons.net>
2020-09-21 10:21:33 -0700

    Updated examples

--- plugins/modules/update_fact.py
+++ plugins/modules/update_fact.py
@@ -43,7 +43,7 @@ author:
 
 EXAMPLES = r"""
 
-# Update, use valid jinja dot or bracket notation
+# Update an exisitng fact, dot or bracket notation
 - name: Set a fact
   set_fact:
     a:
@@ -54,9 +54,15 @@ EXAMPLES = r"""
 
 - name: Update the fact
   update_fact:
-    a.b.c.0: 10
-    a['b']['c'][1]: 20
+    updates:
+    - path: a.b.c.0
+      value: 10
+    - path: "a['b']['c'][1]"
+      value: 20
   register: updated
+
+- debug:
+    var: updated.a
 
 # updated:
 #   a:
@@ -66,7 +72,9 @@ EXAMPLES = r"""
 #       - 20
 #   changed: true
 
-# Update and append to list, add new key
+
+# Lists can be appended, new keys added to dictionaries
+
 - name: Set a fact
   set_fact:
     a:
@@ -74,15 +82,21 @@ EXAMPLES = r"""
         b1:
         - 1
         - 2
-          
+
 - name: Update, add to list, add new key
   update_fact:
-    a.b.b1.2: 3
-    a.b.b2:
-    - 10
-    - 20
-    - 30
+    updates:
+    - path: a.b.b1.2
+      value: 3
+    - path: a.b.b2
+      value:
+      - 10
+      - 20
+      - 30
   register: updated
+
+- debug:
+    var: updated.a
 
 # updated:
 #   a:
@@ -99,7 +113,7 @@ EXAMPLES = r"""
 
 # Retrieve, update, and apply interface description change
 
-- name: Get the config interface config
+- name: Get the current interface config
   cisco.nxos.nxos_interfaces:
     state: gathered
   register: current
@@ -109,8 +123,10 @@ EXAMPLES = r"""
     current: "{{ current['gathered']|rekey_on_member('name') }}"
 
 - name: Update the description of Ethernet1/1
-  update_fact:
-    current.Ethernet1/1.description: "Configured by ansible"
+  ansible.netcommon.update_fact:
+    updates:
+    - path: current.Ethernet1/1.description
+      value: "Configured by ansible"
   register: updated
 
 - name: Update the configuration
@@ -123,57 +139,115 @@ EXAMPLES = r"""
   debug:
     msg: "{{ result['commands'] }}"
 
-# ok: [rtr01] => {
-#     "msg": [
-#         "interface Ethernet1/1",
-#         "description Configured by ansible"
-#     ]
-# }
+# TASK [Show the commands issued] *********
+# ok: [sw01] => 
+#   msg:
+#   - interface Ethernet1/1
+#   - description Configured by ansible
+
 
 # Retrieve, update, and apply an ipv4 ACL change
 
-- name: Get the config acls
+- name: Retrieve the current acls
   arista.eos.eos_acls:
     state: gathered
   register: current
 
-- name: Extract the ipv4 acls
+- name: Retrieve the index of the ipv4 acls
   set_fact:
-    ipv4_acls: "{{ current['gathered']|selectattr('afi', 'equalto', 'ipv4')|list }}"
+    afi_index: "{{ idx }}"
+  loop: "{{ current.gathered }}"
+  loop_control:
+    index_var: idx
+  when: item.afi == 'ipv4'
 
-- name: Rekey the ACLs by name
+- name: Retrieve the index of the test1 acl
   set_fact:
-    ipv4_acls: "{{ ipv4_acls[0]['acls']|rekey_on_member('name') }}"
+    acl_index: "{{ idx }}"
+  loop: "{{ current.gathered[afi_index].acls }}"
+  loop_control:
+    index_var: idx
+  when: item.name == 'test1'
 
-- name: Rekey the test1 ACL by sequence number
+- name: Retrieve the index of sequence 10
   set_fact:
-    aces: "{{ ipv4_acls['test1']['aces']|rekey_on_member('sequence') }}"
+    ace_index: "{{ idx }}"
+  loop: "{{ current.gathered[afi_index].acls[acl_index].aces }}"
+  loop_control:
+    index_var: idx
+  when: item.sequence == 10
 
-- name: Update the source
-  update_fact:
-    aces.10.source:
-      subnet_address: 192.168.1.0/24
+- name: Update the fact
+  ansible.netcommon.update_fact:
+    updates:
+    - path: current.gathered[{{ afi_index }}].acls[{{ acl_index }}].aces[{{ ace_index }}].source
+      value: 
+        subnet_address: "192.168.1.0/24"
   register: updated
 
-- name: Update the device config
+- name: Apply the changes
   arista.eos.eos_acls:
-    config:
-    - afi: ipv4
-      acls:
-      - name: test1
-        aces: "{{ updated['aces'].values()|list }}"
-    state: replaced
+    config: "{{ updated.current.gathered }}"
+    state: overridden
   register: changes
 
-- debug:
-    msg: "{{ changes['commands'] }}"
+- name: Show the commands issued
+  debug:
+    var: changes.commands
 
-# ok: [eos101] => {
-#     "msg": [
-#         "ip access-list test1",
-#         "no 10",
-#         "10 permit ip 192.168.1.0/24 host 10.1.1.2"
-#     ]
-# }
+# TASK [Show the commands issued] ***************
+# ok: [eos101] => 
+#   changes.commands:
+#   - ip access-list test1
+#   - no 10
+#   - ip access-list test1
+#   - 10 permit ip 192.168.20.0/24 host 10.1.1.2
  
+
+# Disable ip_redirects on any layer3 interface
+
+- name: Get the current interface config
+    cisco.nxos.nxos_facts:
+      gather_network_resources:
+      - interfaces
+      - l3_interfaces
+  
+- name: Rekey the interface lists using the interface name
+  set_fact:
+    l3_interfaces: "{{ ansible_network_resources['l3_interfaces']|rekey_on_member('name') }}"
+    update_list: []
+
+- name: Build the update list for any layer3 interface
+  set_fact:
+    update_list: "{{ update_list + update }}"
+  loop: "{{ ansible_network_resources['interfaces'] }}"
+  vars:
+    update:
+    - path: "l3_interfaces[{{ item['name'] }}]['redirects']"
+      value: True
+  when: item['mode']|default() == 'layer3'
+  register: updates
+
+- name: Apply the updates
+  update_fact:
+    updates: "{{ update_list }}"
+  register: updated    
+  
+- name: Update the configuration
+  cisco.nxos.nxos_l3_interfaces:
+    config: "{{ updated.l3_interfaces.values()|list }}"
+    state: overridden
+  register: result
+  when: updated['changed']
+
+- name: Show the commands issued
+  debug:
+    msg: "{{ result['commands'] }}"
+  when: updated['changed']
+
+# TASK [Show the commands issued] *******
+# ok: [sw01] => 
+#   msg:
+#   - interface Ethernet1/10
+#   - no ip redirects
 """
